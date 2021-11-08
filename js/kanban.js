18fffc6e6a3bd603c166390e1055bd5fe8cadd3c
Muthoni Kevin Gatamu <49341231+KevGMR@users.noreply.github.com>
2021-11-08 21:33:37 +0300

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,44 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  const item = document.createElement("div");
+  item.target.classList.add('item');
+  item.id = `item-${order}`;
+  item.draggable = true;
+
+  item.addEventListener('dragstart', (event) => {
+    event.dataTransfer.setData('text', event.target.id);
+  });
+
+  item.addEventListener('dragend', (event) => {
+    event.dataTransfer.clearData();
+  })
+
+  save_btn = document.createElement('button');
+  save_btn.innerHTML = "Save";
+  save_btn.addEventListener('click', () => {
+    error.innerHTML = "";
+    if(input !== ""){
+      order += 1;
+      item.innerHTML = input;
+      adding = false;
+    }else{
+      error.innerHTML = message;
+    }
+
+    item.appendChild(save_btn);
+  })
+  return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    document.getElementById('id') = event.target
+  })
+
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  })
 });
